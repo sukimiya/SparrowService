4d8e135675fdae4bd9f7766fa0758a38903f6ace
周琳琳 <nina.xi@qq.com>
2018-03-30 13:46:27 +0800

    刚刚开始，刚刚开始，呵呵.......

---
+++ src/test/java/io/e2x/sparrow/SparrowApplicationTests.java
@@ -0,0 +1,16 @@
+package io.e2x.sparrow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SparrowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
